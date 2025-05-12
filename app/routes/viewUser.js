3ef2db0f55157ca21717ee566ac5cff9bda38731
steven <61475919+5txvn@users.noreply.github.com>
2025-05-12 17:55:31 -0500

    add view user router and ejs page

---
+++ app/routes/viewUser.js
@@ -0,0 +1,16 @@
+//import modules
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+const User = require('../models/User');
+
+router.get("/:username", async (req, res) => {
+    if(!req.session.username) {
+        res.redirect('/authenticate');
+    } else {
+        const user = await User.findOne({username: req.params.username});
+        res.render(path.join(__dirname, '../views', 'user.ejs'), {user: JSON.stringify(user)});
+    }
+})
+
+module.exports = router;
